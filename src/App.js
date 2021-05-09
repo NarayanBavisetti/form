e41cc6311256854edea603469b020393e478faab
narayan <narayan3119@gmail.com>
2021-05-09 20:57:58 +0530

    first  commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { Box, makeStyles } from '@material-ui/core';
 
+let useStyle = makeStyles({
+  root: {
+    width : "100vw",
+    minHeight: "100vh",
+    background: "#aaaaaa"
+  },
+  headerImg : {
+marginTop: "20px",
+width: "600px",
+height: "150px",
+borderRadius: "15px",
+background:"#ffffff"
+  },
+  inputBox : {
+    marginTop: "20px",
+    width: "600px",
+    height: "150px",
+    borderRadius: "15px",
+    background:"#ffffff"
+      }
+})
 function App() {
+  let classes = useStyle();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<Box className={classes.root}>
+<Box className= {classes.headerImg}></Box>
+<Box className= {classes.inputBox}></Box>
+<Box className= {classes.inputBox}></Box>
+<Box className= {classes.inputBox}></Box>
+</Box>
   );
 }
 
