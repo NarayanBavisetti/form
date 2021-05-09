c5286deab37deb18cfbff1835937f682862423b3
narayan <narayan3119@gmail.com>
2021-05-09 22:09:02 +0530

    new folder

---
+++ index.js
@@ -0,0 +1,28 @@
+let express = require('express')
+let cors = require('cors')
+
+let app = express();
+
+app.use(cors())
+app.use(express.json());
+let PORT = 5000;
+
+let handleGet = function(req, res){
+    //console.log(req)
+    res.send("Welcome to the new world")
+}
+let formSubmission = function(req, res){
+    //console.log(req)
+    let data = req.body;
+    console.log(data)
+    res.send("Submitted")
+}
+
+app.get("/",handleGet);
+app.post("/submit/",formSubmission);
+
+app.listen(PORT,()=>{
+    console.log(`listening to ${PORT}`);
+})
+
+
